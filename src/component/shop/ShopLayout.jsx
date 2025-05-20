edecc16e3559e73f402ff3230c8b8d3b77c79630
pattaraphon1 <klgsa51@gmail.com>
2025-05-20 13:49:36 +0700

    add Link lang to ShopLayout

--- src/component/shop/ShopLayout.jsx
+++ src/component/shop/ShopLayout.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { NavLink, Outlet } from 'react-router'
+import { NavLink, Outlet, useParams } from 'react-router'
 import Card from '../Card'
 
 
 export default function ShopLayout() {
+  const {lang} = useParams();
   return (
     <Card>
       <h1>หมวดหมู่สินค้า</h1>
       <div>
-        <NavLink to ='/shop/electronics'> Electronics |</NavLink>
-        <NavLink to ='/shop/clothing'> Clothing |</NavLink>
-        <NavLink to ='/shop/furniture'> Furniture </NavLink>
+        <NavLink to ={`/${lang}/shop/electronics`}> Electronics |</NavLink>
+        <NavLink to ={`/${lang}/shop/clothing`}> Clothing |</NavLink>
+        <NavLink to ={`/${lang}/shop/furniture`}> Furniture </NavLink>
       </div>
       <Outlet />
     </Card>
